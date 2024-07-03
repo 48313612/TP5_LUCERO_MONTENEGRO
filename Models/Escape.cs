3dd127104d0d98140bc093e408185e48ae485f5e
MARIA EUGENIA LUCERO <48313612@ort>
2024-07-03 14:30:51 -0300

    lol

---
+++ Models/Escape.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP5_LUCERO_MONTENEGRO.Models
+{
+    public class Escape
+    {
+        static private List<string> incognitasSalas = new List<string>();
+    }
+}
