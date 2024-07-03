a66d4b1ba3e61c6e3066aa8d18781eee01158bb8
MARIA EUGENIA LUCERO <48313612@ort>
2024-07-03 13:31:10 -0300

    subiendo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP5_LUCERO_MONTENEGRO.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
