d026f90d508dea0e0489abc29d142bb171175286
MARIA EUGENIA LUCERO <48313612@ort>
2024-07-05 08:12:18 -0300

    cambios

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -7,6 +7,17 @@ namespace TP5_LUCERO_MONTENEGRO.Models
 {
     public class Escape
     {
-        static private List<string> incognitasSalas = new List<string>();
+        static private List<string> incognitasSalas = new List<string>() {"2198", "186", "1111", "agujero negro"};
+        static private int estadoDeJuego = 1;
+
+    private static void InicializarJuego() 
+    {
+
+    }
+  public static int GetEstadoJuego() 
+  {
+    return estadoDeJuego;
+  }
+ //public static bool ResolverSala(int Sala, string Incognita){}
     }
 }
